045ecb08f44647b7887cf46784c9fe07fa9b5a0b
YuanTingHsieh <yhsieh28@wisc.edu>
2018-04-19 02:08:21 -0500

    fix some bugs

--- code/im_complete_opencv.cpp
+++ code/im_complete_opencv.cpp
@@ -118,7 +118,7 @@ struct Box {
 int patch_w  = 8;
 int pm_iters = 5;
 int rs_max   = INT_MAX; // random search
-int sigma = 5 * patch_w * patch_w;
+int sigma = 3 * patch_w * patch_w;
 
 #define XY_TO_INT(x, y) (((y)<<12)|(x))
 #define INT_TO_X(v) ((v)&((1<<12)-1))
@@ -137,7 +137,7 @@ bool isHole(BITMAP *mask, int x, int y) {
 
 /* check if a pixel x, y is in the bounding box or not */
 bool inBox(int x, int y, Box box) {
-  if (x >= box.xmin && x <= box.xmax+patch_w && y >= box.ymin && y <= box.ymax+patch_w) {
+  if (x >= box.xmin && x <= box.xmax && y >= box.ymin && y <= box.ymax) {
     return true;
   }
   return false;
@@ -190,9 +190,11 @@ Box getBox(Mat mask) {
   int xmax = 0, ymax = 0;
   for (int h = 0; h < mask.rows; h++) {
     for (int w = 0; w < mask.cols; w++) {
-      Vec3b mask_pixel = mask.at<Vec3b>(h, w);
+      //Vec3b mask_pixel = mask.at<Vec3b>(h, w);
+      int mask_pixel = (int) mask.at<uchar>(h, w);
       // hole means non-black pixels in mask
-      if (!(mask_pixel[0] == 0 && mask_pixel[1] == 0 && mask_pixel[2] == 0)) {
+      // if (!(mask_pixel[0] == 0 && mask_pixel[1] == 0 && mask_pixel[2] == 0)) {
+      if (mask_pixel == 255) {
           if (h < ymin)
             ymin = h;
           if (h > ymax)
@@ -201,6 +203,8 @@ Box getBox(Mat mask) {
             xmin = w;
           if (w > xmax)
             xmax = w;
+      } else if (mask_pixel != 0) {
+          cout << "SHIT happens, value " << mask_pixel << " in pos x " << w << " , y" << h << endl;
       }
     }
   }
@@ -218,7 +222,7 @@ Box getBox(Mat mask) {
 }
 
 /* Match image a to image b, returning the nearest neighbor field mapping a => b coords, stored in an RGB 24-bit image as (by<<12)|bx. */
-void patchmatch(Mat a, Mat b, Mat mask, BITMAP *&ann, BITMAP *&annd, Box box) {
+void patchmatch(Mat a, Mat b, BITMAP *&ann, BITMAP *&annd, Box box) {
   /* Initialize with random nearest neighbor field (NNF). */
   ann = new BITMAP(a.cols, a.rows);
   annd = new BITMAP(a.cols, a.rows);
@@ -228,7 +232,7 @@ void patchmatch(Mat a, Mat b, Mat mask, BITMAP *&ann, BITMAP *&annd, Box box) {
   memset(ann->data, 0, sizeof(int) * a.cols * a.rows);
   memset(annd->data, 0, sizeof(int) * a.cols * a.rows);
 
-  int box_xmin = box.xmin, box_xmax = box.xmax, box_ymin = box.ymin, box_ymax = box.ymax;
+  //int box_xmin = box.xmin, box_xmax = box.xmax, box_ymin = box.ymin, box_ymax = box.ymax;
 
   int bx, by;
   // Initialization
@@ -253,7 +257,7 @@ void patchmatch(Mat a, Mat b, Mat mask, BITMAP *&ann, BITMAP *&annd, Box box) {
   }
 
   for (int iter = 0; iter < pm_iters; iter++) {
-    printf("  pm_iter = %d\n", iter);
+    // printf("  pm_iter = %d\n", iter);
     /* In each iteration, improve the NNF, by looping in scanline or reverse-scanline order. */
     int ystart = 0, yend = aeh, ychange = 1;
     int xstart = 0, xend = aew, xchange = 1;
@@ -319,40 +323,40 @@ void image_complete(Mat im_orig, Mat mask) {
   // some parameters
   int rows = im_orig.rows;
   int cols = im_orig.cols;
-  //int startscale = (int) -1*ceil(log2(MIN(rows, cols))) + 7;
+  //int startscale = (int) -1*ceil(log2(MIN(rows, cols))) + 8;
   int startscale = -2;
   double scale = pow(2, startscale);
 
   cout << "Scaling image by " << scale << endl;
+
+  double t1 = (double)getTickCount();
  
   // Resize image to starting scale
   Mat resize_img, resize_mask;
   resize(im_orig, resize_img, Size(), scale, scale, INTER_AREA);
   resize(mask, resize_mask, Size(), scale, scale, INTER_AREA);
+  threshold(resize_mask, resize_mask, 127, 255, 0);
 
   // Random starting guess for inpainted image
   rows = resize_img.rows;
   cols = resize_img.cols;
   for (int y = 0; y < rows; ++y) {
     for (int x = 0; x < cols; ++x) {
-      Vec3b mask_pixel = resize_mask.at<Vec3b>(y, x);
+      int mask_pixel = (int) resize_mask.at<uchar>(y, x);
       // if not black pixel, then means white (1) pixel in mask
       // means hole, thus random init colors in hole
-      if (mask_pixel[0] != 0 || mask_pixel[1] !=  0 || mask_pixel[2] != 0) {
+      if (mask_pixel != 0 && mask_pixel != 255) {
+        cout << "FUFUFUFUFUCK" << endl;
+        exit(1);
+      }
+      if (mask_pixel != 0) {
         resize_img.at<Vec3b>(y, x)[0] = rand() % 256;
         resize_img.at<Vec3b>(y, x)[1] = rand() % 256;
         resize_img.at<Vec3b>(y, x)[2] = rand() % 256;
-        mask_pixel[0] = 255;
-        mask_pixel[1] = 255;
-        mask_pixel[2] = 255;
-      } else {
-        mask_pixel[0] = 0;
-        mask_pixel[1] = 0;
-        mask_pixel[2] = 0;
       }
     }
   }
- 
+
   Box ori_box = getBox(mask);
   // debug
   //imwrite("ori_mask.png", mask);
@@ -367,38 +371,52 @@ void image_complete(Mat im_orig, Mat mask) {
   Rect ppap(ori_box.xmin, ori_box.ymin, ori_box.xmax - ori_box.xmin, ori_box.ymax - ori_box.ymin);
   // rectangle(im_orig, ppap, Scalar(255), 2, 8, 0);
 
+
+  double p1 = ((double)getTickCount() - t1) / getTickFrequency();
+  cout << "time for init = " << p1 << endl;
+
   // go through all scale
   for (int logscale = startscale; logscale <= 0; logscale++) {
     scale = pow(2, logscale);
 
     cout << "Scaling is " << scale << endl;
-
-    Mat gray_mask;
-    cvtColor(resize_mask, gray_mask, CV_RGB2GRAY);
    
     Box mask_box = getBox(resize_mask);
  
+    string im_outfile = "im_scale" + to_string(logscale)  + ".png";
+    imwrite(im_outfile, resize_img);
+
     // iterations of image completion
-    int im_iterations = 30;
+    int im_iterations = 40;
     for (int im_iter = 0; im_iter < im_iterations; ++im_iter) {
       printf("im_iter = %d\n", im_iter);
 
       BITMAP *ann = NULL, *annd = NULL;
 
+      double t2 = (double)getTickCount();
+
       Mat B = resize_img.clone();
-      bitwise_and(resize_img, 0, B, gray_mask);
+      bitwise_and(resize_img, 0, B, resize_mask);
 
       // use patchmatch to find NN
-      patchmatch(resize_img, B, resize_mask, ann, annd, mask_box); 
+      patchmatch(resize_img, B, ann, annd, mask_box); 
+
+      //stringstream ss;
+      //ss << im_iter;
+      //string annd_file = "annd_iter_"  + ss.str() + ".jpg";
+      //const char* annd_ptr = annd_file.c_str();
+      //save_bitmap(ann, annd_ptr);
  
+      double p2 = ((double)getTickCount() - t2) / getTickFrequency();
+      cout << "time for PM = " << p2 << endl;
+
+
+      double t3 =  (double)getTickCount();
       // create new image by letting each patch vote
       Mat R = Mat::zeros(resize_img.rows, resize_img.cols, CV_32FC3);
       Mat Rcount = Mat::zeros(resize_img.rows, resize_img.cols, CV_32FC3);
-      //int ew = resize_img.cols - patch_w + 1;
-      //int eh = resize_img.rows - patch_w + 1;
       for (int y = mask_box.ymin; y < mask_box.ymax; ++y) {
         for (int x = mask_box.xmin; x < mask_box.xmax; ++x) {
-          Vec3b mask_pixel = resize_mask.at<Vec3b>(y, x);
             int v = (*ann)[y][x];
             int xbest  = INT_TO_X(v), ybest = INT_TO_Y(v);
             Rect srcRect(Point(x, y), Size(patch_w, patch_w));
@@ -413,22 +431,22 @@ void image_complete(Mat im_orig, Mat mask) {
             toAssign.copyTo(Rcount(srcRect));
 /*
             Mat debugR = Rcount.clone();
-            cout << "Hole (" << x << ", " << y << ") has d " << d <<endl;
-            cout << "Hole (" << x << ", " << y << ") has sim1 " << -d / (2*pow(sigma, 2)) <<endl;
             cout << "Hole (" << x << ", " << y << ") has sim2 " << exp(-d / (2*pow(sigma, 2))) <<endl;
-            cout << "Hole (" << x << ", " << y << ") has sim " << sim <<endl;
             cout << sum(Rcount - debugR) <<endl;
 */
         }
       }
+      double p3 = ((double)getTickCount() - t3) / getTickFrequency();
+      cout << "time for voting = " << p3 << endl;
 
       // normalize new image
+      // COULD BE optimize TODO
       for (int h = 0; h < R.rows; h++) {
         for (int w = 0; w < R.cols; w++) {
           Vec3f rcount_pixel = Rcount.at<Vec3f>(h, w);
-          Vec3f& r_pixel = R.at<Vec3f>(h, w);
             //printf( "Pixel is %d, %d, %d", rc_pixel[0] , rc_pixel[1] , rc_pixel[2] );;
           if (rcount_pixel[0] > 0) {
+            Vec3f& r_pixel = R.at<Vec3f>(h, w);
             r_pixel[0] = (r_pixel[0] / rcount_pixel[0]);
             r_pixel[1] = (r_pixel[1] / rcount_pixel[1]);
             r_pixel[2] = (r_pixel[2] / rcount_pixel[2]);
@@ -443,20 +461,42 @@ void image_complete(Mat im_orig, Mat mask) {
       R.copyTo(resize_img, resize_mask);
 
       // measure how much image has changed, if not much then stop  TODO
-      if (im_iter > 1) {
+      if (im_iter > 0) {
         double diff = 0;
-        for (int h = 0; h < R.rows; h++) {
-          for (int w = 0; w < R.cols; w++) {
-            Vec3b new_pixel = resize_img.at<Vec3b>(h, w);
-            Vec3b old_pixel = old_img.at<Vec3b>(h, w);
-            diff += pow(new_pixel[0] - old_pixel[0], 2);
-            diff += pow(new_pixel[1] - old_pixel[1], 2);
-            diff += pow(new_pixel[2] - old_pixel[2], 2);
+        int mask_count_white = 0;
+        int mask_count_black = 0;
+        int mask_count_other = 0;
+        for (int h = 0; h < resize_img.rows; h++) {
+          for (int w = 0; w < resize_img.cols; w++) {
+            int mask_pixel = (int) resize_mask.at<uchar>(h, w);
+            // white pixel in mask is hole
+            if (mask_pixel == 255) {
+              Vec3b new_pixel = resize_img.at<Vec3b>(h, w);
+              Vec3b old_pixel = old_img.at<Vec3b>(h, w);
+              diff += pow(new_pixel[0] - old_pixel[0], 2);
+              diff += pow(new_pixel[1] - old_pixel[1], 2);
+              diff += pow(new_pixel[2] - old_pixel[2], 2);
+              mask_count_white += 1;
+            } else if (mask_pixel == 0) {
+              mask_count_black += 1;
+            } else {
+              mask_count_other += 1;
+            }
           }
         }
         cout << "diff is " << diff << endl;
+        assert(mask_count_other == 0);
+        //cout << "mask black count is " << mask_count_black << endl;
+        //cout << "mask white count is " << mask_count_white << endl;
+        //cout << "mask other count is " << mask_count_other << endl;
         // diff thres
+        cout << "norm diff is " << diff/mask_count_white << endl;
       }
+
+      string outfile = "r_scale" + to_string(logscale) + "_imiter" + to_string(im_iter) + ".png";
+      imwrite(outfile, R);
+ 
+
       delete ann;
       delete annd;
     }
@@ -464,6 +504,7 @@ void image_complete(Mat im_orig, Mat mask) {
 
     // Upsample A for the next scale
     if (logscale < 0) {
+      double t4 = (double)getTickCount();
       cout << "Upscaling" << endl;
       // orig down scale to new scale
       Mat upscale_img;
@@ -474,24 +515,18 @@ void image_complete(Mat im_orig, Mat mask) {
       resize(resize_img, resize_img, Size(new_cols, new_rows), 0, 0, INTER_CUBIC);
       resize(mask, resize_mask, Size(new_cols, new_rows), 0, 0, INTER_AREA);
 
+      threshold(resize_mask, resize_mask, 127, 255, 0);
+
       Mat inverted_mask;
       bitwise_not(resize_mask, inverted_mask);
       upscale_img.copyTo(resize_img, inverted_mask);
+
+      double p4 = ((double)getTickCount() - t4) / getTickFrequency();
+      cout << "time for resize = " << p4 << endl;
     }
   }
   
   imwrite("final_out.jpg", resize_img);
-}
-
-/*
- * Pass in mask start y, start x and the width and height of that mask
- */
-void createMask(Mat a, int m_x, int m_y, int m_width, int m_height, Mat mask) {
-  int end_x = MIN(a.rows, m_x + m_width);
-  int end_y = MIN(a.cols, m_y + m_height);
-
-  mask(Rect(m_x, m_y, m_width, m_height)) = 255;
-  a(Rect(m_x, m_y, m_width, m_height)) = Vec3b(0, 0, 0);
 }
 
 int main(int argc, char *argv[]) {
@@ -509,22 +544,20 @@ int main(int argc, char *argv[]) {
 
   //createMask(a_matrix, 220, 300, 50, 50, mask_cv);
 
-  Mat mask_cv = imread(argv[1]);
-  /*
+  Mat mask_cv = imread(argv[1], CV_LOAD_IMAGE_GRAYSCALE);
+  
   printf("mask_cv type %d", mask_cv.type());
   for (int y = 0; y < mask_cv.rows; ++y) {
     for (int x = 0; x < mask_cv.cols; ++x) {
-      Vec3b mask_pixel = mask_cv.at<Vec3b>(y, x);
-      if (mask_pixel != Vec3b(0, 0, 0)) {
+      int mask_pixel =(int) mask_cv.at<uchar>(y, x);
+      if (mask_pixel != 0) {
         a_matrix.at<Vec3b>(y, x) = Vec3b(0, 0, 0);
       }
     }
   }
-  */
+  
 
   image_complete(image, mask_cv);
-
-  imwrite("mask_cv.png", mask_cv);
 
   return 0;
 }
